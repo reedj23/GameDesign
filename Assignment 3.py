15758983be7b5e1fd683b55e04f112c02e05cd8f
reedj23 <70218927+reedj23@users.noreply.github.com>
2020-09-21 20:30:36 -0500

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,16 @@
+for line in range(5):
+	print()
+	for number in range(5-line,0,-1):
+		print(number, end =' ')
+
+start = 25
+end = 50
+
+for num in range(start, end + 1):
+        if num > 1:
+                for i in range(2, num):
+                        if (num % i) == 0:
+                                break
+                        else:
+                                print(num)
+                                
