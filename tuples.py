0cf54b76723071c9cb34ed1aa0d4526798b8531a
reedj23 <70218927+reedj23@users.noreply.github.com>
2020-10-05 21:41:07 -0500

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,21 @@
+Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:44:43) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Jack Reed 
+#10 commands using tuples
+
+tuple= (13, 47, 26, 21)
+tuple.count(26)
+coordinates = (4, 5)
+print(coordinates[0])
+
+fruits = ['Bananas','Kiwis', 'Watermelon']
+print(fruits)
+print(fruits[2])
+print(fruits[-2])
+print(fruits[0:2])
+fruists.append('Mangoes')
+fruits.insert(0,'Dragonfruit')
+fruits.pop()
+fruits.reverse()
+fruits.sort()
